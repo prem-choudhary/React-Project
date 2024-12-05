f08790400b85d20b2248ba54406ae1748b2a2fa5
Prem Choudhary <pk5480524@gmail.com>
2024-12-05 18:40:00 +0530

    react-app

--- travel-bucket-list/src/components/DestinationList.js
+++ travel-bucket-list/src/components/DestinationList.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import DestinationItem from './DestinationItem';
+import React from "react";
+import DestinationItem from "./DestinationItem";
 
-function DestinationList({ destinations, setCurrentDestination, removeDestination }) {
+function DestinationList({
+  destinations,
+  setCurrentDestination,
+  removeDestination,
+}) {
   return (
     <div>
       {destinations.map((destination) => (
-        <DestinationItem 
-          key={destination.id} 
-          destination={destination} 
-          setCurrentDestination={setCurrentDestination} 
-          removeDestination={removeDestination} 
+        <DestinationItem
+          key={destination.id}
+          destination={destination}
+          setCurrentDestination={setCurrentDestination}
+          removeDestination={removeDestination}
         />
       ))}
     </div>
